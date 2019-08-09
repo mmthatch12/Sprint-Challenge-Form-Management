fe2b8b179aa1efbf9a92ecb139b8c060b6341117
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-09 09:34:56 -0600

    refactored App.js component as class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super()
 
-    </div>
-  );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello!</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
