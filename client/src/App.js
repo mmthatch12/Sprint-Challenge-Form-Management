a6a2f8dfb5c4900f99c7cceda97aa38cd8ae68c0
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-09 10:27:59 -0600

    working on .get

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios'
+
 import FormicRegistrationForm from './components/RegistrationForm'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      apiData: {}
+    }
+  }
 
+  componentDidMount() {
+    this.getRegistrationData()
+  }
+
+  getRegistrationData = () => {
+    Axios.get("http://localhost:5000/api/restricted/data")
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
 
   render() {
     return (
