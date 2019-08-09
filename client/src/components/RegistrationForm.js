8cbfc8f5e062dfcc766a8e1d269e7ed685ad20e1
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-09 09:59:15 -0600

    finished mapPropsToValues

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -10,7 +10,7 @@ const UForm = () => {
                 <h1>Registration Form</h1>
 
                 <div>
-                    <Field type='test' name='username' placeholder='Username />' />
+                    <Field type='text' name='username' placeholder='Username />' />
 
                     <Field type='password' name='password' placeholder='Password' />
 
@@ -22,7 +22,13 @@ const UForm = () => {
 }
 
 const FormicRegistrationForm = withFormik({
-    
+    mapPropsToValues({ username, password }) {
+        return {
+            username: username || "",
+            password: password || ""
+
+        }
+    }
 })(Uform)
 
 export default UForm
