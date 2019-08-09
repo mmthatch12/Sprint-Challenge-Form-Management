875dbfc1e6716c78fbf09b5edd76f73de2cd6e60
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-09 10:10:19 -0600

    worked on handleSubmit

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,11 @@ import './App.css';
 import FormicRegistrationForm from './components/RegistrationForm'
 
 class App extends React.Component {
-  constructor() {
-    super()
 
-  }
 
   render() {
     return (
       <div className="App">
-        <h1>hello!</h1>
         <FormicRegistrationForm />
       </div>
     );
--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -3,14 +3,17 @@ import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import Axios from 'axios';
 
-const UForm = () => {
+const UForm = ({ errors, touched, values, status }) => {
     return (
         <div>
             <Form>
                 <h1>Registration Form</h1>
 
                 <div>
-                    <Field type='text' name='username' placeholder='Username />' />
+                    <Field type='text' name='username' placeholder='Username' />
+                    {touched.username && errors.username && (
+                        <p>{errors.username}</p>
+                    )}
 
                     <Field type='password' name='password' placeholder='Password' />
 
@@ -33,7 +36,20 @@ const FormicRegistrationForm = withFormik({
     validationSchema: Yup.object().shape({
         name: Yup.string().required('Username field is required'),
         password: Yup.string().min(5, 'Password must be 5 characters or longer').required('Password is required')
-    })
-})(Uform)
+    }),
+
+    handleSubmit(values, { resetForm, setStatus }) {
+        Axios.post("http://localhost:5000/api/register", values)
+            .then(res => {
+                console.log(res)
+                setStatus(res.data)
+                resetForm()
+            })
+            .catch(err => {
+                console.log(err.response)
+            })
+    }
+
+})(UForm)
 
 export default FormicRegistrationForm
