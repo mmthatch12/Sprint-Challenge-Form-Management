17b3cf98b8d45a2abb9d6b0c9e7d224ca9e65419
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-09 11:20:45 -0600

    working on testing

--- client/src/components/GetCard.test.js
+++ client/src/components/GetCard.test.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import GetCard from './GetCard'
 import "@testing-library/react/cleanup-after-each";
+
+import GetCard from './GetCard'
+
+describe("<GetCard />", () => {
+    it("renders without crashing", () => {
+        render(<GetCard />);
+    })
+
+})
